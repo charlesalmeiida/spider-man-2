319514191701e1b724b91bf38dc99980eb824352
charlesalmeiida <charlesalmeida844@gmail.com>
2023-12-06 18:52:18 -0300

    Responsive adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,20 @@ var swiper = new Swiper(".slide-video", {
     nextEl: ".btn-next",
     prevEl: ".btn-prev",
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+      spaceBetween: 16,
+    },
+    767: {
+      slidesPerView: 2.1,
+    },
+    1024: {
+      slidesPerView: 3.1,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
 })
